ea9b76bfc88815e559a3c725fbf592987c39612c
borishdez <borisnandez93@gmail.com>
2024-05-30 18:32:05 -0600

    vista CRUD generada con Blade-bootstrap

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Product
+ *
+ * @property $id
+ * @property $description
+ * @property $price
+ * @property $stock
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Product extends Model
 {
-    use HasFactory;
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['description', 'price', 'stock'];
+
+
 }
